bc2c507f60c36aed84cd21f1debde9caddbdca4b
Adilet Abdraimov <adiletabdraimov@Adilets-MacBook-Pro.local>
2020-09-12 17:11:05 -0400

    Added stages to deploy job

---
+++ JenkinsDeploy.groovy
@@ -0,0 +1,75 @@
+properties([
+    [$class: 'RebuildSettings', autoRebuild: false, rebuildDisabled: false], 
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select to apply all changes to the environment', name: 'applyChanges'), 
+        booleanParam(defaultValue: false, description: 'Please select to destroy all changes to the environment', name: 'destroyChanges'), 
+        string(defaultValue: '', description: 'Please provide the docker to deploy ', name: 'selectedDockerImage', trim: true), 
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please provide the environment to deploy ', name: 'environment')
+        ])
+        ])
+
+
+
+
+
+
+
+// Uniq name for pod/slave
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+
+
+// yaml definiton for slaves
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        
+        - name: fuchicorptools
+          image: fuchicorp/buildtools
+          imagePullPolicy: Always
+          command:
+          - cat
+          tty: true
+
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {
+      node(k8slabel) {
+        stage("Docker check") {
+            container("docker") {
+                sh 'docker --version'
+            }
+        }
+      }
+    }
+
+
+    println(
+    """
+    Apply changes: ${params.applyChanges}
+    Destroy changes: ${params.destroyChanges}
+    Docker  image:  ${params.selectedDockerImage}
+    Environment: ${params.environment}
+    """
+)
